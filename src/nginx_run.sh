dabe0047e634324a3cdbf3e66b9f06c8e22fe590
PipeItToDevNull <piper@dev0.sh>
2023-12-03 19:58:46 +0000

    finalize

--- src/nginx_run.sh
+++ src/nginx_run.sh
@@ -1,29 +1,11 @@
-echo "LOG: Copying configs..."
-cp -rf --verbose /etc/nginx/conf.avail/*.conf /etc/nginx/conf.d/
+echo "LOG: Running py file.."
+python3 /root/nginx_setup.py & pid1=$!
+wait $pid1
 
-echo "LOG: Writing configs from variables..."
+echo "LOG: Validating nginx configurations..."
+nginx -t
 
-python /root/hosts.py
-
-echo "LOG: Running certbot..."
-
-# request our certificates
-if [ "$PRODUCTION" = true ] ; then
-    for host in $(echo $VIRTUAL_HOSTNAMES | tr "," "\n"); \
-        do /usr/bin/certbot --nginx \
-            --non-interactive \
-            --agree-tos --email $EMAIL \
-            --domain $host ; \
-    done
-else
-    for host in $(echo $VIRTUAL_HOSTNAMES | tr "," "\n"); \
-       do /usr/bin/certbot --nginx \
-           --non-interactive \
-           --agree-tos --email $EMAIL \
-           --domain $host --staging ; \
-    done 
-fi
-
+echo "LOG: Setting crontab..."
 # set a crontab renewal of certs
 SLEEPTIME=$(awk 'BEGIN{srand(); print int(rand()*(3600+1))}'); \
     echo "0 0,12 * * * root sleep $SLEEPTIME && certbot renew -q" | tee -a /etc/crontab > /dev/null
@@ -32,7 +14,7 @@ SLEEPTIME=$(awk 'BEGIN{srand(); print int(rand()*(3600+1))}'); \
 crond
 
 if [ -e /var/run/nginx.pid ]; then 
-    echo "nginx is running";
+    echo "LOG: nginx is running, finished...";
 else 
     nginx -g 'daemon off;';
 fi
