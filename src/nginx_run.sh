4b0b546c3723185640d5804ee17ab87dc45fe04b
PipeItToDevNull <piper@dev0.sh>
2023-12-03 21:11:29 +0000

    close #2

--- src/nginx_run.sh
+++ src/nginx_run.sh
@@ -1,11 +1,11 @@
-echo "LOG: Running py file.."
+echo "INFO: Running py file.."
 python3 /root/nginx_setup.py & pid1=$!
 wait $pid1
 
-echo "LOG: Validating nginx configurations..."
+echo "INFO: Validating nginx configurations..."
 nginx -t
 
-echo "LOG: Setting crontab..."
+echo "INFO: Setting crontab..."
 # set a crontab renewal of certs
 SLEEPTIME=$(awk 'BEGIN{srand(); print int(rand()*(3600+1))}'); \
     echo "0 0,12 * * * root sleep $SLEEPTIME && certbot renew -q" | tee -a /etc/crontab > /dev/null
@@ -14,7 +14,7 @@ SLEEPTIME=$(awk 'BEGIN{srand(); print int(rand()*(3600+1))}'); \
 crond
 
 if [ -e /var/run/nginx.pid ]; then 
-    echo "LOG: nginx is running, finished...";
+    echo "INFO: nginx is running, finished...";
 else 
     nginx -g 'daemon off;';
 fi
--- src/nginx_setup.py
+++ src/nginx_setup.py
@@ -1,6 +1,9 @@
 import os
 import json
 import re
+import logging
+
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
 # Take the env var and make it an object (A list of dictionaries)
 hosts_var = os.getenv('HOSTS')
@@ -11,23 +14,23 @@ prod = os.getenv('PRODUCTION')
 
 # copy in any manual conf files the user made
 os.system("cp -rf /etc/nginx/conf.avail/*.conf /etc/nginx/conf.d/ &> /dev/null")
-print("LOG: Configurations present: " + str(os.listdir("/etc/nginx/conf.d")))
+logging.info("Configurations present: " + str(os.listdir("/etc/nginx/conf.d")))
 
 # Basic log printing
-print('LOG: Found: ' + str(len(hosts_json)) + ' hosts')
+logging.info("Found: " + str(len(hosts_json)) + " hosts")
 
 for host in hosts_json:
     for key, value in host.items():
-        print(f"LOG: Found: {key}:{value}")
+        logging.info(f"Found: {key}:{value}")
 
 for host in hosts_json:
-    print("LOG: Working on: " + host['hostname'])
+    logging.info("Working on: " + host['hostname'])
 
     if "hostname" in host:
-        print("LOG: " + host['hostname'] + " is valid")
+        logging.info(host['hostname'] + " is valid")
 
         if "proxy_pass" in host:
-            print("LOG: " + host['hostname'] + " has proxy_pass " + host['proxy_pass'] + " creating conf file")
+            logging.info(host['hostname'] + " has proxy_pass " + host['proxy_pass'] + " creating conf file")
 
             # configuration file creation
             with open('/root/skel.conf') as conf_file:
@@ -35,7 +38,7 @@ for host in hosts_json:
 
             conf_complete = re.sub(r"@@(\w+?)@@", lambda match: host[match.group(1)], conf_contents)
 
-            print("LOG: Writing /etc/nginx/conf.d/" + host['hostname'] + ".conf")
+            logging.info("Writing /etc/nginx/conf.d/" + host['hostname'] + ".conf")
 
             conf_new = open("/etc/nginx/conf.d/" + host['hostname'] + ".conf", "w")
             conf_new.write(conf_complete)
@@ -48,7 +51,7 @@ for host in hosts_json:
                 os.system("/usr/bin/certbot --nginx --non-interactive --agree-tos --email " + email + " --domain " + host['hostname'] + " --staging")
 
         else:
-            print("LOG: " + host['hostname'] + " has no proxy_pass, not creating a conf file")
+            logging.info(host['hostname'] + " has no proxy_pass, not creating a conf file")
 
             # cert requests
             if prod=='true':
@@ -56,5 +59,5 @@ for host in hosts_json:
             else:
                 os.system("/usr/bin/certbot --nginx --non-interactive --agree-tos --email " + email + " --domain " + host['hostname'] + " --staging")
     else:
-        print("LOG: ERROR: Invalid host, I dont know how this happened")
+        logging.error("Invalid host, I dont know how this happened")
         quit()
